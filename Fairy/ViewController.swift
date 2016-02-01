5fe40e30e1652adafaef27cc53a9b692ad97e618
futantan <luckytantanfu@gmail.com>
2016-02-01 12:42:47 +0800

    init project

---
+++ Fairy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fairy
+//
+//  Created by luckytantanfu on 2/1/16.
+//  Copyright © 2016 futantan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
