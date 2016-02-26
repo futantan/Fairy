9e3002830b5331e973b73126ac3b1d418febbbf8
futantan <luckytantanfu@gmail.com>
2016-02-26 16:48:47 +0800

    modify constraints

---
+++ Fairy/ShotDetail/Cell/ShotDescriptionCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShotDescriptionCell.swift
+//  Fairy
+//
+//  Created by luckytantanfu on 2/26/16.
+//  Copyright © 2016 futantan. All rights reserved.
+//
+
+import UIKit
+
+class ShotDescriptionCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
