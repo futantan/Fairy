f9213d5f21f4a9f5fda39e1b3f5eae6e97d008f1
futantan <luckytantanfu@gmail.com>
2016-03-04 21:03:27 +0800

    add RealmManager singleton

---
+++ Fairy/Realm/RealmManger.swift
@@ -0,0 +1,30 @@
+//
+//  RealmManger.swift
+//  Fairy
+//
+//  Created by luckytantanfu on 3/4/16.
+//  Copyright © 2016 futantan. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+private let shredInstance = RealmManager()
+
+class RealmManager {
+  class var sharedManager: RealmManager {
+    return shredInstance
+  }
+  
+  func updateShotsModelInRealm(shotModels: DribbbleShotModel) {
+    try! uiRealm.write {
+      uiRealm.deleteAll()
+      uiRealm.add(shotModels)
+    }
+  }
+  
+  func shotModelsInRealm() -> [DribbbleShotModel] {
+    return Array(uiRealm.objects(DribbbleShotModel))
+  }
+}
